a71727f8cab1560d1ac946d8c375a39dec355b1e
prakruth <8682372+pecom@users.noreply.github.com>
2023-09-27 15:00:00 -0400

    updated form with btk

---
+++ BTK_check.py
@@ -0,0 +1,89 @@
+from abc import ABC, abstractmethod
+
+import astropy.table
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+from dataclasses import dataclass
+from typing import List, Optional, Union
+
+import btk
+import btk.survey
+import btk.draw_blends
+import btk.catalog
+import btk.sampling_functions
+
+import scarlet
+
+class CenteredSampling(btk.sampling_functions.SamplingFunction):
+
+    def __init__(
+        self,
+        max_number=1,
+        min_number=1,
+        stamp_size=24.0,
+        max_shift=0,
+        ndx=0,
+        seed=123
+    ):
+        super().__init__(max_number=max_number, min_number=min_number, seed=seed)
+        self.stamp_size = stamp_size
+        self.max_shift = max_shift if max_shift is not None else self.stamp_size / 10.0
+        self.ndx = ndx
+
+    @property
+    def compatible_catalogs(self):
+        return "CatsimCatalog", "CosmosCatalog"
+
+    def __call__(self, table, shifts=None):
+        blend_table = table[[self.ndx]]
+        blend_table["ra"] = 0.0
+        blend_table["dec"] = 0.0
+
+        out_of_bounds = np.any(blend_table["ra"] > self.stamp_size / 2.0)
+        out_of_bounds = out_of_bounds or np.any(blend_table["dec"] > self.stamp_size / 2.0)
+        if out_of_bounds:
+            warnings.warn("Object center lies outside the stamp")
+        return blend_table
+
+
+catalog_name = "../relfiles/sample_input_catalog.fits"
+catalog = btk.catalog.CatsimCatalog.from_file(catalog_name)
+LSST = btk.survey.get_surveys("LSST")
+allndx = np.array([76, 61, 54, 24, 0, 6])
+allndx = np.array([76])
+batch_size = 10
+stamp_size = 24.0  # Size of the stamp, in arcseconds
+
+
+for ndx in allndx:
+    sampling_function = CenteredSampling(ndx=ndx)
+    
+    draw_generator = btk.draw_blends.CatsimGenerator(
+        catalog,
+        sampling_function,
+        LSST,
+        batch_size=batch_size,
+        stamp_size=stamp_size,
+        njobs=1,
+        add_noise="all",
+    )
+    
+    # generate batch 100 blend catalogs and images.
+    blend_batch = next(draw_generator)
+    
+    deblender = btk.deblend.ScarletDeblend(n=1)
+    deblend_batch = deblender(blend_batch, njobs=4)
+    
+    full_flux = np.einsum('ijk->i', blend_batch.isolated_images[0][0])
+    realization_flux = np.einsum('abcde->ac', deblend_batch.deblended_images)
+    # avg_realization_flux = np.mean(realization_flux, axis=0)
+    scarlet_flux = np.array([np.array(list(scarlet.measure.flux(s[0]).data)) for s in deblend_batch.scarlet_sources])
+    boot_err = np.array([np.std(stats.bootstrap([rf], np.mean).bootstrap_distribution)
+                      for rf in realization_flux.T])
+    fname_prefix = f'galaxy{ndx}_'
+    np.save(f'./output/{fname_prefix}truth', full_flux)
+    np.save(f'./output/{fname_prefix}realizations', realization_flux)
+    np.save(f'./output/{fname_prefix}scarlet', scarlet_flux)
+    np.save(f'./output/{fname_prefix}err', boot_err)
--- gen_blend.py
+++ gen_blend.py
@@ -58,14 +58,14 @@ class CenteredSampling(btk.sampling_functions.SamplingFunction):
             warnings.warn("Object center lies outside the stamp")
         return blend_table
 
-class PairSampling(btk.sampling_functions.SamplingFunction):
+class DoubleSampling(btk.sampling_functions.SamplingFunction):
     """
     BTK function to place a galaxy at the center of the postage stamp
-    and another nearby (depends on self.setshift).
-    
-    Note this currently uses the same galaxy at both locations.
+    and another nearby (distance depends on self.setshift).
+
+    The ndx[0] object is placed at the center.
     """
-    def __init__(self, stamp_size=24.0, maxshift=None, setshift=None, ndx=24):
+    def __init__(self, stamp_size=24.0, maxshift=None, setshift=None, ndx=[24, 23]):
         super().__init__(2)
         self.stamp_size = stamp_size
         self.maxshift = maxshift if maxshift else self.stamp_size / 10.0
@@ -83,7 +83,7 @@ class PairSampling(btk.sampling_functions.SamplingFunction):
 
     def __call__(self,table):
 
-        indexes = [self.ndx,self.ndx]
+        indexes = self.ndx
         blend_table = table[indexes]
 
         blend_table["ra"] = 0.0
@@ -103,7 +103,7 @@ class PairSampling(btk.sampling_functions.SamplingFunction):
             warnings.warn("Object center lies outside the stamp")
         return blend_table
 
-def main_single(df, ndx=4, verbose=False, scale_size=1, catalog_name=None):
+def main_single(df, ndx=4, verbose=False, bsize=10, catalog_name=None):
     """
     Create mock 'blends' with one galaxy pasted at the center 
     of the postage stamp.
@@ -116,15 +116,11 @@ def main_single(df, ndx=4, verbose=False, scale_size=1, catalog_name=None):
     if catalog_name is None:
         catalog_name = "../catalogs/sample_input_catalog.fits"
     stamp_size = 24
-    bsize = 10
-    surveys = btk.survey.get_surveys("LSST")
+    surveys = btk.survey.get_surveys("Rubin")
     bands = list('ugrizy')
 
     catalog = btk.catalog.CatsimCatalog.from_file(catalog_name)
-    catalog.table['a_d'] *= scale_size
-    catalog.table['b_d'] *= scale_size
-    catalog.table['a_b'] *= scale_size
-    catalog.table['b_b'] *= scale_size
+        
     # sampling_function = btk.sampling_functions.DefaultSampling(max_number=5)
     sampling_function = CenteredSampling(setndx=ndx)
     draw_blend_generator = btk.draw_blends.CatsimGenerator(
@@ -151,15 +147,89 @@ def main_single(df, ndx=4, verbose=False, scale_size=1, catalog_name=None):
         noisy_im = blend_images[bs]
         noiseless_im = blend_iso[bs]
         blend_cat = blend_list[bs]
+        
+        full_flux = np.einsum('ijk->i', noiseless_im[0])
 
 
         df["cata_ndx"].append(ndx)
-        df["noiseless_image"].append(noiseless_im)
+        df["run_ndx"].append(bs)
+        df["noiseless_image"].append(noiseless_im[0])
+        df["full_flux"].append(full_flux)
         df["noisy_image"].append(noisy_im)
-        df["true_x"].append(blend_cat['x_peak'])
-        df["true_y"].append(blend_cat['y_peak'])
 
     if verbose:
         print("Appended data ...")
  
     print(f"Done with galaxy {ndx}")
+
+# def main_double(df, ndx=[4,5], verbose=False, scale_size=1, catalog_name=None, shift=5):
+#     """
+#     Create mock blends with one galaxy pasted at the center 
+#     of the postage stamp and another galaxy nearby.
+
+#     df - should be a dictionary that gets modified directly in the function (dictionary object)
+#     ndx - Catalog indexes of galaxy that will be pasted (list of integer)
+#     verbose - Include extra print statements or not (bool)
+#     scale_size - Increase size of galaxy parameters (float)
+#     shift - Displacement of second galaxy from center (float)
+#     """
+    
+#         # This hasn't been cleaned or tested.
+
+        
+#     if catalog_name is None:
+#         catalog_name = "../catalogs/sample_input_catalog.fits"
+#     stamp_size = 24
+#     bsize = 10
+#     surveys = btk.survey.get_surveys("LSST")
+#     bands = list('ugrizy')
+
+#     catalog = btk.catalog.CatsimCatalog.from_file(catalog_name)
+#     catalog.table['a_d'] *= scale_size
+#     catalog.table['b_d'] *= scale_size
+#     catalog.table['a_b'] *= scale_size
+#     catalog.table['b_b'] *= scale_size
+#     # sampling_function = btk.sampling_functions.DefaultSampling(max_number=5)
+#     sampling_function = DoubleSampling(ndx=ndx, setshift=shift)
+#     draw_blend_generator = btk.draw_blends.CatsimGenerator(
+#         catalog,
+#         sampling_function,
+#         surveys,
+#         stamp_size=stamp_size,
+#         batch_size=bsize,
+#         cpus=1,
+#         add_noise='all'
+#     )
+#     if verbose:
+#         print("Created blend generator...")
+
+#     batch = next(draw_blend_generator)
+#     if verbose:
+#         print("Created blends...")
+#     blend_images = batch['blend_images']
+#     assert(blend_images[0][0].shape==(120,120))
+#     blend_list = batch['blend_list']
+#     blend_iso = batch['isolated_images']
+
+#     for bs in range(bsize):
+#         noisy_im = blend_images[bs]
+#         noiseless_im = blend_iso[bs]
+#         blend_cat = blend_list[bs]
+
+
+#         df["cata_ndx"].append(ndx)
+#         df["run_ndx"].append(bs)
+#         df["noiseless_image"].append(noiseless_im)
+#         df["noisy_image"].append(noisy_im)
+#         df["true_x"].append(blend_cat['x_peak'])
+#         df["true_y"].append(blend_cat['y_peak'])
+
+#     if verbose:
+#         print("Appended data ...")
+ 
+#     print(f"Done with galaxy {ndx}")
+
+def get_schema():
+    dframe_keys = ["cata_ndx", "run_ndx", "noisy_image", "noiseless_image", "full_flux"]
+    dframe_solo = {sk:[] for sk in dframe_keys}
+    return dframe_solo
