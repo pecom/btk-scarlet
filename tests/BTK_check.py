71c3557605b9323cf19b0a222021cd623dcb47aa
Prakruth Adari <padari@jamaica.astro.sunysb.edu>
2023-10-18 15:08:47 -0400

    updated btk test file

--- tests/BTK_check.py
+++ tests/BTK_check.py
@@ -19,6 +19,14 @@ import os
 ddir = os.getenv("DATADIR")
 odir = os.getenv("OUTDIR")
 
+rng = np.random.default_rng()
+
+if odir=='':
+    print("Need to set output directory!")
+
+if ddir=='':
+    print("Need to set data directory!")
+
 class CenteredSampling(btk.sampling_functions.SamplingFunction):
 
     def __init__(
@@ -55,9 +63,14 @@ catalog_name = f"{ddir}/input_catalog.fits"
 catalog = btk.catalog.CatsimCatalog.from_file(catalog_name)
 LSST = btk.survey.get_surveys("LSST")
 allndx = np.array([76, 61, 54, 24, 0, 6])
-allndx = np.array([76])
+# allndx = np.array([76])
 batch_size = 10
 stamp_size = 24.0  # Size of the stamp, in arcseconds
+
+full_flux = []
+realization_flux = []
+scarlet_flux = []
+boot_err = []
 
 
 for ndx in allndx:
@@ -75,20 +88,32 @@ for ndx in allndx:
     
     # generate batch 100 blend catalogs and images.
     blend_batch = next(draw_generator)
+    blend_batch.save_fits(f'{odir}/btk_check')
     
     deblender = btk.deblend.Scarlet(max_n_sources=1, max_iter=1000)
     deblend_batch = deblender(blend_batch, njobs=1)
+    deblend_batch.save_fits(f'{odir}/btk_check')
 
+    # Double check that SEX output and BTK+SEP give the same thing for aperture photometry
     
-    full_flux = np.einsum('ijk->i', blend_batch.isolated_images[0][0])
-    realization_flux = np.einsum('abcde->ac', deblend_batch.deblended_images)
+    full_flux.append(np.einsum('ijk->i', blend_batch.isolated_images[0][0]))
+    real_flux = np.einsum('abcde->ac', deblend_batch.deblended_images)
+    realization_flux.append(real_flux)
     # avg_realization_flux = np.mean(realization_flux, axis=0)
-    scarlet_flux = np.array([np.array(list(scarlet.measure.flux(s['scarlet_sources'][0]).data)) 
-                         for s in deblend_batch.extra_data])
-    boot_err = np.array([np.std(stats.bootstrap([rf], np.mean).bootstrap_distribution)
-                      for rf in realization_flux.T])
-    fname_prefix = f'galaxy{ndx}_'
-    np.save(f'{odir}/btk_tests/{fname_prefix}truth', full_flux)
-    np.save(f'{odir}/btk_tests/{fname_prefix}realizations', realization_flux)
-    np.save(f'{odir}/btk_tests/{fname_prefix}scarlet', scarlet_flux)
-    np.save(f'{odir}/btk_tests/{fname_prefix}err', boot_err)
+    scarlet_flux.append(np.array([np.array(list(scarlet.measure.flux(s['scarlet_sources'][0]).data))
+                         for s in deblend_batch.extra_data]))
+    boot_err.append(np.array([np.std(stats.bootstrap([rf], np.mean).bootstrap_distribution)
+                      for rf in real_flux.T]))
+
+
+fname_prefix = f'check1_'
+full_flux = np.array(full_flux)
+realization_flux = np.array(realization_flux)
+scarlet_flux = np.array(scarlet_flux)
+boot_err = np.array(boot_err)
+
+np.save(f'{odir}/btk_check/{fname_prefix}truth', full_flux)
+np.save(f'{odir}/btk_check/{fname_prefix}realizations', realization_flux)
+np.save(f'{odir}/btk_check/{fname_prefix}scarlet', scarlet_flux)
+np.save(f'{odir}/btk_check/{fname_prefix}err', boot_err)
+np.save(f'{odir}/btk_check/{fname_prefix}allndx', allndx)
